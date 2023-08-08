1b51d56420bf8f6b65e0ab92dee1195c14f8e22a
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-08-08 14:26:17 +0900

    [Feat] 404페이지 처리

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,19 @@ import SignUpPage from './pages/SignUpPage';
 import SignInPage from './pages/SignInPage';
 import TodoPage from './pages/TodoPage';
 import { HomeContainer, HomeDiv } from './styled-components/Home-styled';
+import NotFound from './pages/NotFound';
+
 function App() {
   return (
     <HomeContainer>
       <HomeDiv>
         <BrowserRouter>
           <Routes>
-            <Route path='/' element={<HomePage />}></Route>
-            <Route path='/signin' element={<SignInPage/>}></Route>
-            <Route path='/signup' element={<SignUpPage/>}></Route>
-            <Route path='/todo' element={<TodoPage/>}></Route>
+            <Route path='/' element={<HomePage />}/>
+            <Route path='/signin' element={<SignInPage/>}/>
+            <Route path='/signup' element={<SignUpPage/>}/>
+            <Route path='/todo' element={<TodoPage/>}/>
+            <Route path='/*' element={<NotFound/>}/>
           </Routes>
         </BrowserRouter>
       </HomeDiv>
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFound() {
+
+    return(
+        <div>
+            올바른 페이지 경로가 아닙니다.
+        </div>
+    )
+}
+
+export default NotFound;
