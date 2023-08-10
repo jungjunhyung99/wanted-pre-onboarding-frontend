1ecdf7714b7fd7a675165e980aebd11a70373f98
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-08-11 00:41:37 +0900

    [Style] 로그인 스타일 추가

--- src/styled-components/SignIn-styled.js
+++ src/styled-components/SignIn-styled.js
@@ -4,6 +4,7 @@ export const SignInContainer = styled.div`
     display:flex;
     flex-direction: column;
     align-items: center;
+    gap: 3rem;
 `;
 
 export const Form = styled.form`
@@ -15,9 +16,22 @@ export const Form = styled.form`
 
 export const Input = styled.input`
     width: 20rem;
+    border-radius: 2rem;
+    padding-left: 1rem;
     height: 2rem;
 `;
 
 export const Button = styled.button`
-    width: 5rem;
+    width: 8rem;
+    font-size: 1.5rem;
+    border: 3px solid black;
+    background-color: white;
+    border-radius: 1rem;
+    
 `;
+
+export const ButtonDiv = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-evenly;
+`
